e9499c9742ca52df7ac259717509bbff816414c3
James Moore <jameslmoore@james-moores-macbook.local>
2017-02-23 21:39:18 -1000

    first commit

--- js/app.js
+++ js/app.js
@@ -2,3 +2,5 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+menuElement = document.getElementsByClassName("menu");
+console.log(menuElement)
