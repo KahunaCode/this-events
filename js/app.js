5e1bdc7d3fae30972b838a3a83aed2283ed563ed
James Moore <jameslmoore@james-moores-macbook.local>
2017-02-23 21:53:27 -1000

    added event listeners

--- js/app.js
+++ js/app.js
@@ -2,5 +2,20 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
-menuElement = document.getElementsByClassName("menu");
-console.log(menuElement)
+menuElement = document.getElementsByClassName("name");
+//console.log(menuElement)
+
+for(var i = 0; i<menuElement.length; i++){
+	menuElement[i].addEventListener("click", showMenu);
+}
+
+
+function showMenu(){
+ var pic = this.querySelectorAll(".menu")[0];
+  console.log("clicky")
+  if(pic.style.display === "none"){
+    pic.style.display = "block";
+  }else{
+    pic.style.display = "none";
+  }
+}
